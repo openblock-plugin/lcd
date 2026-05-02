70ed0dd785aefec99990c0a0d54260d664bc6dea
Arthur Zheng <arthurzheng150@gmail.com>
2026-05-02 13:19:45 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# 1602 LCD
+
+![Version](https://img.shields.io/github/v/tag/openblock-plugin/lcd?label=version)
+![License](https://img.shields.io/badge/license-MIT-green.svg)
+![Type](https://img.shields.io/badge/type-extension-orange.svg)
+![Tags](https://img.shields.io/badge/tags-display-lightgrey.svg)
+
+1602 liquid crystal display which based on I2C bus.
+
+## Documentation
+
+For detailed usage instructions, please visit:
+
+**[OpenBlock Wiki](https://wiki.openblock.cc)**
---
+++ src/translations.js
@@ -0,0 +1,107 @@
+/* eslint-disable quote-props */
+/* eslint-disable max-len */
+/**
+ * Translation file for this resouce.
+ *
+ * IMPORTANT:
+ * - The "en" (English) section is automatically generated from source files.
+ *   Do NOT modify the "en" section manually.
+ * - Other language sections (e.g., "zh-cn", "zh-tw", "ja", etc.) should be
+ *   manually translated by you.
+ * - When you run the extraction script again, only the "en" section will be
+ *   updated. Your manual translations in other languages will be preserved.
+ *
+ * Structure:
+ * - interface: translations for name/description (used by GUI formatMessage)
+ * - extensions: translations for extension blocks (used by VM formatMessage)
+ * - blocks: translations for Blockly blocks (used by Blockly.Msg)
+ */
+
+export default {
+    'interface': {
+        'en': {
+            'lcd.name': '1602 LCD',
+            'lcd.description': '1602 liquid crystal display which based on I2C bus.'
+        },
+        'ru': {
+            'lcd.name': '1602 LCD',
+            'lcd.description': '1602 liquid crystal display which based on I2C bus.'
+        },
+        'zh-cn': {
+            'lcd.name': '1602 LCD',
+            'lcd.description': '1602 liquid crystal display which based on I2C bus.'
+        },
+        'zh-tw': {
+            'lcd.name': '1602 LCD',
+            'lcd.description': '1602 liquid crystal display which based on I2C bus.'
+        }
+    },
+    'extensions': {
+        'en': {},
+        'ru': {},
+        'zh-cn': {},
+        'zh-tw': {}
+    },
+    'blocks': {
+        'en': {
+            'LCD_CATEGORY': '1602 LCD',
+            'LCD_INIT': 'init lcd I2C address %1',
+            'LCD_SETCURSORPOSITION': 'set lcd cursor position x: %1 y: %2',
+            'LCD_PRINT': 'lcd print %1',
+            'LCD_CLEAR': 'clear lcd',
+            'LCD_SETBACKLIGHT': 'set lcd back light %1',
+            'LCD_SATE_ON': 'on',
+            'LCD_SATE_OFF': 'off',
+            'LCD_SETCURSORSTYLE': 'set lcd cursor %1 style %2',
+            'LCD_SATE_DISPLAY': 'display',
+            'LCD_SATE_HIDE': 'hide',
+            'LCD_STYLE_BLINK': 'blink',
+            'LCD_STYLE_NOBLINK': 'no-blink'
+        },
+        'ru': {
+            'LCD_CATEGORY': '1602 LCD',
+            'LCD_INIT': 'init lcd I2C address %1',
+            'LCD_SETCURSORPOSITION': 'set lcd cursor position x: %1 y: %2',
+            'LCD_PRINT': 'lcd print %1',
+            'LCD_CLEAR': 'clear lcd',
+            'LCD_SETBACKLIGHT': 'set lcd back light %1',
+            'LCD_SATE_ON': 'on',
+            'LCD_SATE_OFF': 'off',
+            'LCD_SETCURSORSTYLE': 'set lcd cursor %1 style %2',
+            'LCD_SATE_DISPLAY': 'display',
+            'LCD_SATE_HIDE': 'hide',
+            'LCD_STYLE_BLINK': 'blink',
+            'LCD_STYLE_NOBLINK': 'no-blink'
+        },
+        'zh-cn': {
+            'LCD_CATEGORY': '1602 LCD',
+            'LCD_INIT': 'init lcd I2C address %1',
+            'LCD_SETCURSORPOSITION': 'set lcd cursor position x: %1 y: %2',
+            'LCD_PRINT': 'lcd print %1',
+            'LCD_CLEAR': 'clear lcd',
+            'LCD_SETBACKLIGHT': 'set lcd back light %1',
+            'LCD_SATE_ON': 'on',
+            'LCD_SATE_OFF': 'off',
+            'LCD_SETCURSORSTYLE': 'set lcd cursor %1 style %2',
+            'LCD_SATE_DISPLAY': 'display',
+            'LCD_SATE_HIDE': 'hide',
+            'LCD_STYLE_BLINK': 'blink',
+            'LCD_STYLE_NOBLINK': 'no-blink'
+        },
+        'zh-tw': {
+            'LCD_CATEGORY': '1602 LCD',
+            'LCD_INIT': 'init lcd I2C address %1',
+            'LCD_SETCURSORPOSITION': 'set lcd cursor position x: %1 y: %2',
+            'LCD_PRINT': 'lcd print %1',
+            'LCD_CLEAR': 'clear lcd',
+            'LCD_SETBACKLIGHT': 'set lcd back light %1',
+            'LCD_SATE_ON': 'on',
+            'LCD_SATE_OFF': 'off',
+            'LCD_SETCURSORSTYLE': 'set lcd cursor %1 style %2',
+            'LCD_SATE_DISPLAY': 'display',
+            'LCD_SATE_HIDE': 'hide',
+            'LCD_STYLE_BLINK': 'blink',
+            'LCD_STYLE_NOBLINK': 'no-blink'
+        }
+    }
+};
